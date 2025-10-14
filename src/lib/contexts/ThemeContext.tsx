5cf7b339774e6aa81bb58fcbfc16204e4d3b7e2d
sjmitch-git <sjmitch20@outlook.com>
2025-10-14 09:52:34 +0100

    Add TOC

--- src/lib/contexts/ThemeContext.tsx
+++ src/lib/contexts/ThemeContext.tsx
@@ -1,44 +1,55 @@
-"use client";
+'use client';
 
-import { createContext, useContext, useState, useEffect, useMemo, ReactNode } from "react";
-import { Theme, ThemeContextType } from "@types";
+import {
+  createContext,
+  useContext,
+  useState,
+  useEffect,
+  useMemo,
+  ReactNode,
+} from 'react';
+import { Theme, ThemeContextType } from '@types';
 
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
   const [theme, setTheme] = useState<Theme>(() => {
-    if (typeof window === "undefined") return "light";
-    const storedTheme = localStorage.getItem("theme");
-    if (storedTheme === "light" || storedTheme === "dark") {
+    if (typeof window === 'undefined') return 'dark';
+    const storedTheme = localStorage.getItem('theme');
+    if (storedTheme === 'light' || storedTheme === 'dark') {
       return storedTheme as Theme;
     }
-    return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    return window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark'
+      : 'light';
   });
 
   const toggleTheme = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTheme(e.target.checked ? "dark" : "light");
+    setTheme(e.target.checked ? 'dark' : 'light');
   };
 
   useEffect(() => {
-    localStorage.setItem("theme", theme);
-    document.documentElement.setAttribute("data-theme", theme);
-    if (theme === "dark") {
-      document.body.classList.add("dark");
+    localStorage.setItem('theme', theme);
+    document.documentElement.setAttribute('data-theme', theme);
+    if (theme === 'dark') {
+      document.body.classList.add('dark');
     } else {
-      document.body.classList.remove("dark");
+      document.body.classList.remove('dark');
     }
   }, [theme]);
 
   const value = useMemo(() => ({ theme, toggleTheme }), [theme]);
 
-  return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>;
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
 };
 
 export const useTheme = () => {
   const context = useContext(ThemeContext);
   if (!context) {
     throw new Error(
-      "useTheme must be used within a ThemeProvider. Ensure ThemeProvider wraps your app."
+      'useTheme must be used within a ThemeProvider. Ensure ThemeProvider wraps your app.'
     );
   }
   return context;
